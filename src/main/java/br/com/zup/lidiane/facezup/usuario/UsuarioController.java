baaf7805b6ce1ea5e2e46dba84d5c4e9aa56bb76
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-25 16:47:01 -0300

    Criar Classe Controller de Usuario

---
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioController.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.facezup.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+}
