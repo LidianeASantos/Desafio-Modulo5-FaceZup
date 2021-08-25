7ab5f77394fb0024fb6afc08438c3cce17d1f864
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-25 18:53:18 -0300

    Criar método Conversor

--- src/main/java/br/com/zup/lidiane/facezup/componentes/Conversor.java
+++ src/main/java/br/com/zup/lidiane/facezup/componentes/Conversor.java
@@ -1,11 +1,13 @@
 package br.com.zup.lidiane.facezup.componentes;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Conversor {
 
+    @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
