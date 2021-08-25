31ffcb507fe544b4f1dfffb3a6c87a52c5490470
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-25 16:38:57 -0300

    Criar Classe Service de Usuario

---
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioService.java
@@ -0,0 +1,11 @@
+package br.com.zup.lidiane.facezup.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+}
