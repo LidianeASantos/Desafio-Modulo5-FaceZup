4be6c0314ecc0b114a9b7ba1877019c00eebe85c
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-25 16:44:04 -0300

    Criar método para Cadastrar Usuario

--- src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioService.java
@@ -8,4 +8,8 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrarUusario(Usuario usuario){
+        return usuarioRepository.save( usuario );
+    }
 }
