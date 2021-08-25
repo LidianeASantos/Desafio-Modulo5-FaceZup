894fec3634d713dfcf513b3aea6840e56ae73f59
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-25 15:25:49 -0300

    Setup do Projeto

---
+++ src/main/java/br/com/zup/lidiane/facezup/FacezupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.facezup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacezupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacezupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/lidiane/facezup/FacezupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.facezup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacezupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
