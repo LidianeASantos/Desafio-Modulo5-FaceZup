b62ad039bfb6c1690f9fc4c5e9a25489c4e4b638
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-26 16:50:42 -0300

    Criar método para verificar ususrio exixtente

--- src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioService.java
@@ -1,7 +1,10 @@
 package br.com.zup.lidiane.facezup.usuario;
 
+import br.com.zup.lidiane.facezup.mensagem.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class UsuarioService {
@@ -12,4 +15,10 @@ public class UsuarioService {
     public Usuario cadastrarUsario(Usuario usuario){
         return usuarioRepository.save( usuario );
     }
+
+    public boolean contatoExistente(String id) {
+        return usuarioRepository.existsById(id);
+    }
+
+
 }
