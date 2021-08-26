b27d3333b9355033f3d9f957e9b88492e430cb1e
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-26 14:04:39 -0300

    Implementar cadastro de usuario no controller

--- src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioController.java
@@ -1,8 +1,11 @@
 package br.com.zup.lidiane.facezup.usuario;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -11,6 +14,20 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario cadastrarUsuario(@RequestBody @Valid UsuarioDto usuarioDTO){
+        Usuario usuarioModel = modelMapper.map(usuarioDTO, Usuario.class);
+
+       return this.usuarioService.cadastrarUsario( usuarioModel );
+
+    }
+
 
 
 }
