2f1f7845d664e5437b3f0308210a9cac148b5a2d
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-28 15:53:07 -0300

    Refatorar classe usuario

--- src/main/java/br/com/zup/lidiane/facezup/usuario/Usuario.java
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/Usuario.java
@@ -1,31 +1,40 @@
 package br.com.zup.lidiane.facezup.usuario;
 
 import br.com.zup.lidiane.facezup.mensagem.Mensagem;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "usuario")
+@Table(name = "USUARIO")
+@JsonInclude
 public class Usuario {
 
-
-
-    @Column(nullable = false, length = 50)
-    private String nome;
-    @Column(nullable = false, length = 50)
-    private String sobreNome;
     @Id
     @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 50)
+    private String sobreNome;
+
     @Column(nullable = false, length = 50)
     private String cargo;
 
-
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @OneToMany(mappedBy = "usuario")
     private List<Mensagem> mensagens;
 
-    public Usuario() {
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getNome() {
@@ -44,19 +53,19 @@ public class Usuario {
         this.sobreNome = sobreNome;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getCargo() {
         return cargo;
     }
 
     public void setCargo(String cargo) {
         this.cargo = cargo;
+    }
+
+    public List<Mensagem> getMensagens() {
+        return mensagens;
+    }
+
+    public void setMensagens(List<Mensagem> mensagens) {
+        this.mensagens = mensagens;
     }
 }
