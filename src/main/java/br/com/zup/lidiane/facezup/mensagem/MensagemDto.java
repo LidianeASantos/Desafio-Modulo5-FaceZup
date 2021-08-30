a86a1b4785d1b6c48c5f3fe099e4133b6d963c18
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 10:08:59 -0300

    Acrescentar anotação menssagem dto

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemDto.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemDto.java
@@ -1,19 +1,43 @@
 package br.com.zup.lidiane.facezup.mensagem;
 
+import br.com.zup.lidiane.facezup.usuario.Usuario;
+import br.com.zup.lidiane.facezup.usuario.UsuarioDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@JsonInclude
 public class MensagemDto {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long id;
     @NotBlank
     private String mensagem;
+
     @Email(message = "{validacao.destino.mensagem}")
     private String destino;
+
     @Email(message = "{validacao.origem.mensagem}")
     private String origem;
 
 
-    public MensagemDto() {
+    private boolean visualizado;
+
+    public Boolean getVisualizado() {
+        return visualizado;
+    }
+
+    public void setVisualizado(Boolean visualizado) {
+        this.visualizado = visualizado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getMensagem() {
@@ -39,4 +63,33 @@ public class MensagemDto {
     public void setOrigem(String origem) {
         this.origem = origem;
     }
+
+
+
+    public MensagemDto() {
+    }
+
+    public Mensagem converterDTOParaModel() {
+        Mensagem mensagem = new Mensagem();
+        mensagem.setMensagem( this.mensagem );
+        mensagem.setOrigem( this.origem );
+        mensagem.setDestino( this.destino );
+
+        return mensagem;
+    }
+
+
+    public static MensagemDto converterModelParaDTO(Mensagem mensagem) {
+        MensagemDto mensagemDTO = new MensagemDto();
+        UsuarioDto usuarioDto = UsuarioDto.converterModelParaDTO(mensagem.getUsuario());
+
+        mensagemDTO.setId(mensagem.getId() );
+        mensagemDTO.setOrigem(mensagem.getOrigem());
+        mensagemDTO.setDestino( mensagem.getDestino());
+        mensagemDTO.setMensagem(mensagem.getMensagem());
+        mensagemDTO.setVisualizado(mensagem.isVisualizado());
+
+        return mensagemDTO;
+    }
+
 }
