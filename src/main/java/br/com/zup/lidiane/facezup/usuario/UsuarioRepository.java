7414e428941ee7b911387ab9f7247cccfd044def
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 10:33:19 -0300

    Criar optional na classe usuário repository

--- src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/lidiane/facezup/usuario/UsuarioRepository.java
@@ -2,7 +2,10 @@ package br.com.zup.lidiane.facezup.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, String> {
 
 
+    Optional<Usuario> findById(String email);
 }
