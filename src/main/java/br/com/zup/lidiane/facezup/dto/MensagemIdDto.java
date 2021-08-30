dc03ef9930a78df8adc248de598be62aa2d5bc1e
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 11:02:18 -0300

    Criar classe dto para mensagem id

---
+++ src/main/java/br/com/zup/lidiane/facezup/dto/MensagemIdDto.java
@@ -0,0 +1,17 @@
+package br.com.zup.lidiane.facezup.dto;
+
+public class MensagemIdDto {
+
+    private Long id;
+
+    public MensagemIdDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
