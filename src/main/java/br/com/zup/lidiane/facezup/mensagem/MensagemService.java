b0feef70f112425683e2d7727a108e6385aa9233
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 10:11:10 -0300

    Refatorar método de cadastro de mensagem

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemService.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemService.java
@@ -1,9 +1,13 @@
 package br.com.zup.lidiane.facezup.mensagem;
 
 import br.com.zup.lidiane.facezup.usuario.Usuario;
+import br.com.zup.lidiane.facezup.usuario.UsuarioRepository;
 import br.com.zup.lidiane.facezup.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
 
 
 @Service
@@ -11,28 +15,19 @@ public class MensagemService {
 
     @Autowired
     private MensagemRepository mensagemRepository;
-
     @Autowired
     private UsuarioService usuarioService;
 
 
-    public Mensagem cadastrarMensagem(String origem, String destino, String mensagem) {
-            Mensagem novaMensagem = new Mensagem();
+    public Mensagem cadastrarMensagem(String email, Mensagem mensagem) {
 
-        if (usuarioService.usuarioExistente( origem ) & usuarioService.usuarioExistente( destino )) {
+        Usuario usuario = usuarioService.buscarUsuarioPeloId( email );
 
-            Usuario usuarioOrigem = usuarioService.buscarUsuarioPeloId( origem );
-            Usuario usuarioDestino = usuarioService.buscarUsuarioPeloId( destino );
+        mensagem.setUsuario(usuario);
+        mensagem.setVisualizado( false );
 
-            novaMensagem.setUsuario( usuarioOrigem );
-            novaMensagem.setUsuario( usuarioDestino );
-            novaMensagem.setMensagem( mensagem );
-            novaMensagem.setVisualizado( false );
-
-            mensagemRepository.save( novaMensagem );
-        }
-        throw new RuntimeException("Usuário não encontrado!");
-
+        return mensagemRepository.save( mensagem );
     }
+
 
 }
