1f1acd3b87e71d2b731edbe631f2f4e7bb89ddba
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 20:25:04 -0300

    Alterar método quantidade de mensagen na classe mensagem repository

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
@@ -2,11 +2,8 @@ package br.com.zup.lidiane.facezup.mensagem;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface MensagemRepository extends CrudRepository<Mensagem, Long> {
 
-    List<Mensagem> findAllByVizualizadoFalseAndDestinoUsuarioemail(String emailUsuario);
-    Integer countAllByVisualizadoFalseAndDestinoUsuarioEmail(String emailUsuario);
+    long countByVisualizado(Boolean visualizado);
 
 }
