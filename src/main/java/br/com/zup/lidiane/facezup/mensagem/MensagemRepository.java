c6a7852ae83ba296966449ff941277dae078b83a
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 10:18:47 -0300

    Excluir booleano mensagm repository

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
@@ -3,4 +3,6 @@ package br.com.zup.lidiane.facezup.mensagem;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MensagemRepository extends CrudRepository<Mensagem, Long> {
+
+
 }
