01699fa90e8504b1516c1f55334eb8f97ed71b48
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 11:52:07 -0300

    Criar classe usuario mensagem dto

---
+++ src/main/java/br/com/zup/lidiane/facezup/dto/UsuarioMensagemIdDto.java
@@ -0,0 +1,18 @@
+package br.com.zup.lidiane.facezup.dto;
+
+public class UsuarioMensagemIdDto {
+
+    private Long id;
+
+    public UsuarioMensagemIdDto(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
