dac2b0c629c6cc9c6ba61443cba5436d5c529ccd
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-30 11:11:15 -0300

    Criar find all na classe mensagem repository

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
@@ -2,7 +2,10 @@ package br.com.zup.lidiane.facezup.mensagem;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MensagemRepository extends CrudRepository<Mensagem, Long> {
 
+    List<Mensagem> findAllByVizualizadoFalseAndDestinoUsuarioemail(String emailUsuario);
 
 }
