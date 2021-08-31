a2a4f858a177a601fd34523c66780d860d3314ad
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-31 11:40:05 -0300

    Refatorar código mensagem reposytory

--- src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
+++ src/main/java/br/com/zup/lidiane/facezup/mensagem/MensagemRepository.java
@@ -2,8 +2,11 @@ package br.com.zup.lidiane.facezup.mensagem;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MensagemRepository extends CrudRepository<Mensagem, Long> {
 
-    long countByVisualizado(Boolean visualizado);
+    long countByVisualizadoAndDestino(Boolean visualizado, String email);
 
+    Optional<Mensagem> findById(String email);
 }
