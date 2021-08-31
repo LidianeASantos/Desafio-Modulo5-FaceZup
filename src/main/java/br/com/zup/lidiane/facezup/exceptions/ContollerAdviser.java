2cd1f2e372132d3f54592cdafe3ab22db09255d4
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-08-31 18:17:09 -0300

    Criar classe controller adviser para tratar as exceções

---
+++ src/main/java/br/com/zup/lidiane/facezup/exceptions/ContollerAdviser.java
@@ -0,0 +1,27 @@
+package br.com.zup.lidiane.facezup.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ContollerAdviser {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MensagemDeErro manipularExcecoesDeValidacao(MethodArgumentNotValidException exception){
+        List<FieldError> fildErros = exception.getBindingResult().getFieldErrors();
+
+        List<Erro> erros = fildErros.stream().map(objeto -> new Erro(objeto.getDefaultMessage()))
+                .collect( Collectors.toList());
+
+        return new MensagemDeErro(400, erros);
+    }
+
+}
